cafac149b2d9c313a2ea221886b8cfb9a4dfe26a
rafitinan <rafitinan@gmail.com>
2024-06-09 01:29:48 +0600

    Initial commit

---
+++ web.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import functions
+
+todos= functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+
+st.title("My To-do App")
+st.subheader("be more productive than ever")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+st.text_input(label= '', placeholder="Type in a new to-do",
+              on_change=add_todo, key='new_todo')
+
+
+
+
+(st.session_state)
